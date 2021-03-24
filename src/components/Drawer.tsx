a2494a9b7a0f8108ba61daa26460e71498d00d4c
Masashi Kawafuji <masashikawafuji@gmail.com>
2021-03-24 20:44:53 +0900

    A mask appears whilst the drawer is opening.

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -5,13 +5,16 @@ type DrawerProps = {
 };
 
 const Drawer: React.FC<DrawerProps> = ({ children, isOpen }) => (
-  <div
-    className={`${
-      !isOpen && '-left-full'
-    } sm:block sm:static transition-all duration-500 fixed z-10 left-0 inset-y-0 w-3/4 sm:w-full px-4 pt-4 bg-gray-900`}
-  >
-    {children}
-  </div>
+  <>
+    {isOpen && <div className="fixed inset-0 z-10 bg-gray-900 bg-opacity-50" />}
+    <div
+      className={`${
+        !isOpen && '-left-full'
+      } fixed left-0 inset-y-0 sm:block sm:static z-20 transition-all duration-500 w-3/4 sm:w-full px-4 pt-4 bg-gray-900`}
+    >
+      {children}
+    </div>
+  </>
 );
 
 export default Drawer;
